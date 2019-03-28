46d0ca56b7c0e81a548bed8a651eb4161a10782d
spoorthis26 <48976601+spoorthis26@users.noreply.github.com>
2019-03-28 12:05:42 +0530

    Create my_calc.c

---
+++ my_calc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+#include "my_calc.h"
+
+int
+calculator(int m, int a, int b)
+{
+  if (m==1)
+    {
+      return a+b;
+    }
+  else
+   {
+   int r=pow(a,b);
+   }
+  return 0;
+}
