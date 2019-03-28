73be46373c1c85706df0679686effc7a994251e4
spoorthis26 <48976601+spoorthis26@users.noreply.github.com>
2019-03-28 12:16:02 +0530

    Update assert.c

--- assert.c
+++ assert.c
@@ -7,17 +7,17 @@ void test_cal(void)
 {
   printf ("Test calculator using assert:      ");
   assert (calculator(1,5,1)==6);
-  assert (max(14,13)==14);
-  /* assert (max(INT_MAX,14)==INT_MAX); */
-  /* assert (max(INT_MAX,INT_MIN)==INT_MAX); */
-  /* assert (max(-23000,INT_MIN)==-23000); */
+  assert (calculator(1,-5,3)==-2);
+  assert (calculator(1,-5,-3)==-8);
+  assert (calculator(1,5,-3)==2);
+  assert (calculator(0,5,3)==125);
+  assert (calculator(0,-5,3)==0);
+  assert (calculator(0,5,-3)==0.008);
   printf (" OK\n");
 }
 
 int
 main(void)
 {
-  test_max();
-
-  test_mean();
+  test_cal();
 }
